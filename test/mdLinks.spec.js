933348d324b3bec3a7233c28b138ef4b28d79ca3
Ania pq <melanpq@gmail.com>
2023-01-11 08:14:15 -0500

    test alas funciones pathIsFile y pathIsDirectory

--- test/mdLinks.spec.js
+++ test/mdLinks.spec.js
@@ -1,5 +1,10 @@
 
-import { pathValidator, pathAbsolute } from "../src/functionsAll.js";
+import {
+  pathValidator,
+  pathAbsolute,
+  pathIsDirectory,
+  pathIsFile
+} from "../src/functionsAll.js";
 import { mdLinks } from "../src/mdLinks.js";
 
 describe('mdLinks', () => {
@@ -36,5 +41,32 @@ describe('pathAbsolute', () => {
   });
    it("should return path absolute", () => {
     expect(pathAbsolute('./proof/datos.text')).toBe('C:\\Users\\USUARIO\\laboratoria\\proyect4\\DEV001-md-links-aniapq\\proof\\datos.text');
+  });
+});
+
+/** test  FUNCTION:pathIsFile, */
+describe('pathIsFile', () => {
+
+  it("is a function", () => {
+    expect(typeof pathIsFile).toBe("function");
+  });
+  it("should return TRUE", () => {
+    expect(pathIsFile('README.md')).toBeTruthy();
+  });
+   it("should return FALSE", () => {
+    expect(pathIsFile('./proof')).toBeFalsy();
+  });
+});
+/** test  FUNCTION:pathIsDirectory */
+describe('pathIsDirectory', () => {
+
+  it("is a function", () => {
+    expect(typeof pathIsDirectory).toBe("function");
+  });
+  it("should return TRUE", () => {
+    expect(pathIsDirectory('./proof')).toBeTruthy();
+  });
+   it("should return FALSE", () => {
+    expect(pathIsDirectory('README.md')).toBeFalsy();
   });
 });
