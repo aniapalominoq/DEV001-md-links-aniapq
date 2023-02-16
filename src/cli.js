bde9e753e7d9a0657f250009461b09b8f5e9f7c9
Ania pq <melanpq@gmail.com>
2023-02-15 20:28:17 -0500

    edicion de README

--- src/cli.js
+++ src/cli.js
@@ -1,139 +1,131 @@
 #!/usr/bin/env node
-import colors from 'colors'
-import { table } from 'table'
-import { contentHelp, menu, pause, readInput} from './viewUser.js'
-import { mdLinks } from './mdLinks.js'
-import { brokenLinks, fullLinks, uniqueLinks } from './functionsAll.js'
 
+import colors from "colors";
+import { table } from "table";
+import { contentHelp, menu, pause, readInput } from "./viewUser.js";
+import { mdLinks } from "./mdLinks.js";
+import { brokenLinks, fullLinks, uniqueLinks } from "./functionsAll.js";
 
 const mainOptions = async () => {
-  let opt = ''
-  const ruta = await readInput()
+  let opt = "";
+  const ruta = await readInput();
   do {
-    opt = await menu()
+    opt = await menu();
     switch (opt) {
-
-      case '1':
-         console.clear()
-             mdLinks(ruta, { validate: false })
-            .then(res => {
-              const title = [['HREF'.yellow, 'TEXT'.yellow, 'PATH'.yellow]]
-              const dato = title.concat(res.map(elem => Object.values(elem)))
-              const config = {
-                columns: {
-                  0: { width: 40 },
-                  2: { width: 55 },
-               
-                },
-                header: {
-                  alignment: 'center',
-                  content: 'Table with basic information'.yellow,
-                },
-              }
-              console.log('\n');
-              console.log(colors.grey(table(dato, config)))
-            
-            })
-            .catch(error => console.log(error))
-
+      case "1":
+        console.clear();
+        mdLinks(ruta, { validate: false })
+          .then((res) => {
+            const title = [["HREF".yellow, "TEXT".yellow, "PATH".yellow]];
+            const dato = title.concat(res.map((elem) => Object.values(elem)));
+            const config = {
+              columns: {
+                0: { width: 40 },
+                2: { width: 55 },
+              },
+              header: {
+                alignment: "center",
+                content: "Table with basic information".yellow,
+              },
+            };
+            console.log("\n");
+            console.log(colors.grey(table(dato, config)));
+          })
+          .catch((error) => console.log(error));
 
         break;
-      case '2':
-       console.clear()
-          mdLinks(ruta, { validate: true })
-            .then(res => {
-              const title = [['HREF'.yellow, 'TEXT'.yellow, 'PATH'.yellow, 'STATUS'.yellow, 'OK'.yellow]]
-              const dato = title.concat(res.map(elem => Object.values(elem)))
-              const config = {
-                columns: {
-                  0: { width: 40 },
-                  2: { width: 55 },
-               
-                },
-                header: {
-                  alignment: 'center',
-                  content: '--validate'.yellow,
-                },
-              }
-              console.log('\n');
-              console.log(colors.grey(table(dato, config)))
-            
-            })
-            .catch(error => console.log(error))
-       
-        break;
-      case '3':
-        console.clear()
-
-         mdLinks(ruta,{validate:false})
-           .then(res => { 
-               const dato = [
-             ['Total',`${fullLinks(res)}`],
-             ['unique',`${uniqueLinks(res)}`]
-           
-          ]
-           
-           const config = {
-             columns: {
-               1: { width: 20 },
-               
-             },
-            header: {
-            alignment: 'center',
-            content: '--stats'.yellow,
-  },
-           }
-           console.log('\n');
-         console.log(colors.grey(table(dato, config)))
-            })
-          .catch(error=>console.log(error))
-        break;
-      case '4':
-        console.clear()
-        console.log('\n');
-       mdLinks(ruta,{validate:true})
-         .then(res => {
-           
-           const dato = [
-             ['Total',`${fullLinks(res)}`],
-             ['unique',`${uniqueLinks(res)}`],
-             ['Broken',`${brokenLinks(res)}`.red]
-          ]
-           
-           const config = {
-             columns: {
-               1: { width: 10},
-               
-             },
-            header: {
-            alignment: 'center',
-            content: '--stats --validate'.yellow,
-  },
-           }
-           console.log('\n');
-           console.log(colors.grey(table(dato, config)))
-           
-         })
-          .catch(error => console.log(error))
-        break;
-      
-      case '5':
-        console.clear()
-        contentHelp()
+      case "2":
+        console.clear();
+        mdLinks(ruta, { validate: true })
+          .then((res) => {
+            const title = [
+              [
+                "HREF".yellow,
+                "TEXT".yellow,
+                "PATH".yellow,
+                "STATUS".yellow,
+                "OK".yellow,
+              ],
+            ];
+            const dato = title.concat(res.map((elem) => Object.values(elem)));
+            const config = {
+              columns: {
+                0: { width: 40 },
+                2: { width: 55 },
+              },
+              header: {
+                alignment: "center",
+                content: "--validate".yellow,
+              },
+            };
+            console.log("\n");
+            console.log(colors.grey(table(dato, config)));
+          })
+          .catch((error) => console.log(error));
 
         break;
-      case '0':
-     console.clear()
+      case "3":
+        console.clear();
+
+        mdLinks(ruta, { validate: false })
+          .then((res) => {
+            const dato = [
+              ["Total", `${fullLinks(res)}`],
+              ["unique", `${uniqueLinks(res)}`],
+            ];
+
+            const config = {
+              columns: {
+                1: { width: 20 },
+              },
+              header: {
+                alignment: "center",
+                content: "--stats".yellow,
+              },
+            };
+            console.log("\n");
+            console.log(colors.grey(table(dato, config)));
+          })
+          .catch((error) => console.log(error));
+        break;
+      case "4":
+        console.clear();
+        console.log("\n");
+        mdLinks(ruta, { validate: true })
+          .then((res) => {
+            const dato = [
+              ["Total", `${fullLinks(res)}`],
+              ["unique", `${uniqueLinks(res)}`],
+              ["Broken", `${brokenLinks(res)}`.red],
+            ];
+
+            const config = {
+              columns: {
+                1: { width: 10 },
+              },
+              header: {
+                alignment: "center",
+                content: "--stats --validate".yellow,
+              },
+            };
+            console.log("\n");
+            console.log(colors.grey(table(dato, config)));
+          })
+          .catch((error) => console.log(error));
+        break;
+
+      case "5":
+        console.clear();
+        contentHelp();
+
+        break;
+      case "0":
+        console.clear();
         break;
     }
-   console.log('\n');
-    await pause()
-    
-  } while (opt != 0)
- 
-} 
+    console.log("\n");
+    await pause();
+  } while (opt != 0);
+};
 
-
-mainOptions()
- 
-
+mainOptions();
--- src/viewUser.js
+++ src/viewUser.js
@@ -1,8 +1,7 @@
-import inquirer from 'inquirer'
-import colors from 'colors'
-import { table } from 'table';
-import figlet from 'figlet';
-
+import inquirer from "inquirer";
+import colors from "colors";
+import { table } from "table";
+import figlet from "figlet";
 
 let date_ob = new Date();
 // current date
@@ -13,46 +12,44 @@ let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 // current year
 let year = date_ob.getFullYear();
 
-
-
-
 export const menu = async () => {
-  const questions = [{
-  type: 'list',
-  name: 'options',
-  message: 'What do you want to do, with the path entered?'.yellow,
-    choices: [
-  {
-      value: '1',
-      name: `--default`.grey
-    },
+  const questions = [
     {
-      value: '2',
-      name: `--validate`.grey
-    },
-    {
-      value: '3',
-      name: `--stats`.grey
-    },
-    {
-      value: '4',
-      name: `--stats --validate`.grey
-
-      },
-       {
-        value: '5',
-        name:`--help`.grey
-      },
+      type: "list",
+      name: "options",
+      message: "What do you want to do, with the path entered?".yellow,
+      choices: [
         {
-        value: '0',
-        name: `--exit`.red, 
-      }
-    ]
-}] 
+          value: "1",
+          name: `--default`.grey,
+        },
+        {
+          value: "2",
+          name: `--validate`.grey,
+        },
+        {
+          value: "3",
+          name: `--stats`.grey,
+        },
+        {
+          value: "4",
+          name: `--stats --validate`.grey,
+        },
+        {
+          value: "5",
+          name: `--help`.grey,
+        },
+        {
+          value: "0",
+          name: `--exit`.red,
+        },
+      ],
+    },
+  ];
 
-  const { options} = await inquirer.prompt(questions)
-  return options
-}
+  const { options } = await inquirer.prompt(questions);
+  return options;
+};
 
 /* export const tableValidate = async () => {
   const questions = [{
@@ -69,68 +66,76 @@ export const menu = async () => {
 
 export const contentHelp = () => {
   const data = [
-   
-    ['Options'.yellow, 'Result'.yellow],
-    ['--validate'.grey,'Print href, text, file, ok(ok or fail) and status.'.white],
-    ['--stats'.grey, 'Print total and unique links.'.white],
-    ['--stats --validate'.grey, 'Print total, unique and broken links.'.white],
-    ['--help'.grey,  'Show all the options.'.white],
-    ['--exit'.grey, 'Exit the program.'.white],
-  ]
-   const config = {
-  columns: {
-    1: { width: 60 }
-  },
- 
-  }
-  return console.log(colors.grey(table(data, config)))
-}
- 
+    ["Options".yellow, "Result".yellow],
+    [
+      "--validate".grey,
+      "Print href, text, file, ok(ok or fail) and status.".white,
+    ],
+    ["--stats".grey, "Print total and unique links.".white],
+    ["--stats --validate".grey, "Print total, unique and broken links.".white],
+    ["--help".grey, "Show all the options.".white],
+    ["--exit".grey, "Exit the program.".white],
+  ];
+  const config = {
+    columns: {
+      1: { width: 60 },
+    },
+  };
+  return console.log(colors.grey(table(data, config)));
+};
+
 export const readInput = async () => {
-  console.clear()
-  console.log(`${'∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞ '}`.yellow);
-   console.log(colors.yellow(figlet.textSync('maskay-links-md')));
-  console.log(`${'∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞ \n'}`.yellow);
+  console.clear();
+  console.log(
+    `${"∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞ "}`
+      .yellow
+  );
+  console.log(colors.yellow(figlet.textSync("maskay-links-md")));
+  console.log(
+    `${"∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞-∞ \n"}`
+      .yellow
+  );
 
-  console.log(`                                                           today : ${date}-${month}-${year}`.yellow);
-  if (process.env.USERNAME) {
-    console.log('Hello,'.yellow, `${process.env.USERNAME}`.green )
+  console.log(
+    `                                                           today : ${date}-${month}-${year}`
+      .yellow
+  );
+  if (process.env.USERNAM) {
+    console.log("Hello,".yellow, `${process.env.USERNAME}`.green);
+  } else if (process.env.USER) {
+    console.log("Hello,".yellow, `${process.env.USER}`.green);
+  } else {
+    console.log("Hello,".yellow, `(*^_^*)`.green);
   }
-  else if (process.env.USER) {
-    console.log('Hello,'.yellow, `${process.env.USER}`.green )
-  }
-    else {
-     console.log('Hello,'.yellow,`(*^_^*)`.green)
-    }
-  console.log('-------------------------------------------------------------------------------------'.grey)
- const inputs = [
-   {
-     type: 'input',
-    name: 'filePath',
-      message:`Enter the path of a file`.yellow,
-    
-    validate(value) {
-        if (value.length === 0) return 'Enter the path of a file'.yellow  
-     return true
-      }
-  }]
+  console.log(
+    "-------------------------------------------------------------------------------------"
+      .grey
+  );
+  const inputs = [
+    {
+      type: "input",
+      name: "filePath",
+      message: `Enter the path of a file`.yellow,
 
+      validate(value) {
+        if (value.length === 0) return "Enter the path of a file".yellow;
+        return true;
+      },
+    },
+  ];
 
-   const {filePath} = await inquirer.prompt(inputs)
-   return filePath
-  }
-
+  const { filePath } = await inquirer.prompt(inputs);
+  return filePath;
+};
 
 export const pause = async () => {
   const question = [
     {
-      type: 'input',
-      name: 'enter',
-      message: `Press ${'enter'.green} to continue...`. yellow,
-    }
+      type: "input",
+      name: "enter",
+      message: `Press ${"enter".green} to continue...`.yellow,
+    },
   ];
- 
-  await inquirer.prompt(question)
- 
-}
 
+  await inquirer.prompt(question);
+};
